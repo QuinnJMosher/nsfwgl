5fd79638bf3d23fc49a90e7646fc0c6f484366ee
QuinnJMosher <Darkstaro59@gmail.com>
2015-10-13 15:50:49 -0700

    implement renderer

--- nsfwgl/render.cpp
+++ nsfwgl/render.cpp
@@ -1,18 +1,48 @@
 #include "nsfw.h"
-
+#include "gl_core_4_4.h"
 
 // Each Case should set up a uniform using the pass parameters
 bool nsfw::RenderPass::setUniform(const char *name, nsfw::UNIFORM::TYPE type, const void *value, unsigned count, bool normalize) 
 {
+	unsigned loc = glGetUniformLocation(*shader, name);
+
+	float* castFloat;
+	castFloat = (float*)value;
+	int* castInt;
+	castInt = (int*)value;
+
 	switch (type)
 	{
-	case nsfw::UNIFORM::FLO1: TODO_D("Setup float uniform!");     break;
-	case nsfw::UNIFORM::FLO3: TODO_D("Setup vec3 uniform!");      break;
-	case nsfw::UNIFORM::FLO4: TODO_D("Setup vec4 uniform!");      break;
-	case nsfw::UNIFORM::MAT4: TODO_D("Setup mat4 uniform!");	  break;
-	case nsfw::UNIFORM::INT1: TODO_D("Setup integer uniform!");	  break;
-	case nsfw::UNIFORM::TEX2: TODO_D("Setup texture2D uniform!"); break;
-	default:				  TODO_D("INVALID Uniform type.");	  break;
+	case nsfw::UNIFORM::FLO1: 
+		glUniform1f(*shader, *castFloat);
+		/*TODO_D("Setup float uniform!");*/
+		break;
+	case nsfw::UNIFORM::FLO3:
+		glUniform3fv(*shader, 1, castFloat);
+		//TODO_D("Setup vec3 uniform!");
+		break;
+	case nsfw::UNIFORM::FLO4:
+		glUniform4fv(*shader, 1, castFloat);
+		//TODO_D("Setup vec4 uniform!");      
+		break;
+	case nsfw::UNIFORM::MAT4:
+		glUniformMatrix4fv(*shader, 1, GL_FALSE, castFloat);
+		//TODO_D("Setup mat4 uniform!");	  
+		break;
+	case nsfw::UNIFORM::INT1: 
+		glUniform1i(*shader, *castInt);
+		//TODO_D("Setup integer uniform!");	  
+		break;
+	case nsfw::UNIFORM::TEX2: 
+		glUniform1i(*shader, *castInt);
+		//TODO_D("Setup texture2D uniform!"); 
+		break;
+	default:
+		#ifdef _DEBUG
+		std::cerr << "an invalid uniform type has been passed for item: " << name << std::endl;
+		#endif
+		//TODO_D("INVALID Uniform type.");	  
+		break;
 	}
 
 	return false;
